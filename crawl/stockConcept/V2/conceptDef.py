e81bf14b5e3653f8fc0c280c0a22a809799708c6
wishyoulikebefore <37919975+wishyoulikebefore@users.noreply.github.com>
2018-06-02 08:09:49 +0800

    Update conceptDef.py

--- crawl/stockConcept/V2/conceptDef.py
+++ crawl/stockConcept/V2/conceptDef.py
@@ -1,24 +1,30 @@
-import os
-from stockMongo import stockConcept_mongo
+from stockMongo_class import StockConceptMongo,SubNewStockMongo
 from selenium import webdriver
-from commonUse import getHtmlPage_GBK,judge_date
+from commonUse import getHtmlPage_GBK
 import time
 import multiprocessing
+from commonUse import myMail
+
+"""
+2018.5.31更新：
+次新股的概念根据爬虫结果设定，不再更具jqk的结果，在stockConcept_mongo中单独设置
+由于pymongo的key不能出现"."，对于工业4.0这种，必须把"."去掉
+
+有待更新：概念黑名单，有些概念非常垃圾或者过时
+"""
 
 def startCrawl(conceptType,url):
-    """
-    jqkConcept：http://q.10jqka.com.cn/gn/
-
-    regionConcept：http://q.10jqka.com.cn/dy/
-    industryConcept：http://q.10jqka.com.cn/thshy/
-    """
+    blackList = ["新股与次新股","首发新股","参股360","参股新三板","ST板块","万达私有化","债转股","杭州亚运会","马云概念","腾讯概念","王者荣耀","摘帽"]
     targetDict = {}
     soup = getHtmlPage_GBK(url)
     content = soup.find("div", class_="cate_inner").find_all("a")
     for item in content:
         conceptUrl = item["href"]
-        conceptName = item.get_text()
-        targetDict[conceptName] = conceptUrl
+        conceptName = item.get_text().replace(".","")
+        if conceptName not in blackList:
+            targetDict[conceptName] = conceptUrl
+        else:
+            pass
     return targetDict
 
 def crawl_page(conceptName,conceptUrl,conceptType):
@@ -46,7 +52,7 @@ def crawl_stock(browser,conceptName,conceptType):
     for item in locate:
         stockCode = item.find_elements_by_xpath("td")[1].text
         stockName = item.find_elements_by_xpath("td")[2].text
-        stockConcept_mongo().updateConcept(stockCode, stockName, conceptType, conceptName)
+        StockConceptMongo().update(stockCode, stockName, conceptType, conceptName)
 
 if __name__ == "__main__":
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
@@ -59,3 +65,9 @@ if __name__ == "__main__":
             pool.apply_async(crawl_page, (conceptName, targetDict.get(conceptName), key,))
     pool.close()
     pool.join()
+    subNewMongo = SubNewStockMongo(delta=180, name="newstock")
+    subNewMongo.update()
+    for item in subNewMongo.collection.find():
+        stockCode = item["stockCode"]
+        StockConceptMongo().collection.update({"stockCode": stockCode}, {"$set": {"subNewStock": True}})
+    myMail("概念更新","已完成")
